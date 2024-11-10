2d69c44cc18fdbd6db1dbd56e5f567c575c1e960
Mathéo <matheo.piget5@gmail.com>
2024-11-10 14:07:30 +0100

    rajout de la gestion des pipes,

--- src/parser.c
+++ src/parser.c
@@ -4,9 +4,107 @@
 #include "../include/execute.h"
 #include "../include/builtin.h" // Si les fonctions builtin_* sont déclarées ici
 #include "../include/command.h" // Si printerr est déclarée ici
+#include <sched.h>
+#include <unistd.h>
 
 int parse_and_execute(int argc, char **argv)
 {
+
+    // Gestion des pipes multiples
+    int pipe_count = 0;
+    for (int i = 0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "|") == 0)
+        {
+            pipe_count++;
+        }
+    }
+
+    if (pipe_count > 0)
+    {
+        int pipefds[2 * pipe_count];
+        for (int i = 0; i < pipe_count; i++)
+        {
+            if (pipe(pipefds + i * 2) == -1)
+            {
+                printt("pipe");
+                printt(": error creating pipe\n");
+                return 1;
+            }
+        }
+
+        int command_start = 0;
+        int pipe_index = 0;
+        for (int i = 0; i <= argc; i++)
+        {
+            if (i == argc || strcmp(argv[i], "|") == 0)
+            {
+                argv[i] = NULL; // Séparer les commandes
+
+                pid_t pid = fork();
+                if (pid == -1)
+                {
+                    printt("fork");
+                    printt(": error fork\n");
+                    return 1;
+                }
+
+                if (pid == 0)
+                {
+                    // Rediriger stdin si ce n'est pas la première commande
+                    if (pipe_index > 0)
+                    {
+                        if (dup2(pipefds[(pipe_index - 1) * 2], STDIN_FILENO) == -1)
+                        {
+                            printt("dup2");
+                            printt(": error dup2\n");
+                            return 1;
+                        }
+                    }
+
+                    // Rediriger stdout si ce n'est pas la dernière commande
+                    if (pipe_index < pipe_count)
+                    {
+                        if (dup2(pipefds[pipe_index * 2 + 1], STDOUT_FILENO) == -1)
+                        {
+                            printt("dup2");
+                            printt(": error dup2\n");
+                            return 1;
+                        }
+                    }
+
+                    // Fermer tous les descripteurs de pipe
+                    for (int j = 0; j < 2 * pipe_count; j++)
+                    {
+                        close(pipefds[j]);
+                    }
+
+                    execvp(argv[command_start], &argv[command_start]);
+                    printt("execvp");
+                    printt(": error execvp\n");
+                    return 1;
+                }
+
+                command_start = i + 1;
+                pipe_index++;
+            }
+        }
+
+        // Fermer tous les descripteurs de pipe dans le processus parent
+        for (int i = 0; i < 2 * pipe_count; i++)
+        {
+            close(pipefds[i]);
+        }
+
+        // Attendre tous les processus enfants
+        for (int i = 0; i <= pipe_count; i++)
+        {
+            wait(NULL);
+        }
+
+        return 0;
+    }
+
     // Commandes internes
     if (argc == 0)
     {
