30ac6e9f3a2698d5c8e238cfe3067da16c23fdcd
Nidhal Moussa <nidhalmoussa@etu.u-paris.fr>
2024-11-11 16:38:26 +0100

    aezgez

--- src/piped_commands.c
+++ src/piped_commands.c
@@ -10,6 +10,7 @@
 int parse_and_execute_pipe(int argc, char **argv)
 {
     // Commandes internes
+    char output[1076];
     for (size_t i = 0; i < argc; i++)
     {
         if (strcmp(argv[i], "pwd") == 0)
@@ -58,15 +59,22 @@ int parse_and_execute_pipe(int argc, char **argv)
                 return builtin_ftype(filename);
             }
         }
-        return execute_command(argv[i], argv);
+        return execute_command(argv[i],output, argv);
     }
 }
-int execute_command(const char *command, char **argv)
+int execute_command(const char *command,char *output, char **argv)
 {
     pid_t pid = fork();
-    
+    int pipefd[2];
+    if (pipe(pipefd)==-1)
+    {
+        perror("pipe");
+        return 1;
+    }
     if (pid == 0)
     { // Processus enfant
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
         execvp(command, argv);
         printt(command);
         printt(": command not found\n");
@@ -81,6 +89,12 @@ int execute_command(const char *command, char **argv)
     }
     else
     { // Processus parent
+        if (read(pipefd[0],output,strlen(output))==-1);
+        {
+            perror("read");
+            return 1;
+        }
+        
         int status;
         waitpid(pid, &status, 0);
         if (WIFEXITED(status))
