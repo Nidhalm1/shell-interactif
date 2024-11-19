10fac644cde69e0297b45f8d2e0c8395938b8804
Mathéo <matheo.piget5@gmail.com>
2024-11-19 16:08:33 +0100

    commande structuré + ordre des priorité dans parser

--- src/parser.c
+++ src/parser.c
@@ -26,14 +26,17 @@ int parse_and_execute(int argc, char **argv)
         return parse_and_execute_simple(argc, argv);
     }
 
-    // Parcours des arguments pour détecter un pipe (|) et rediriger l'exécution
-    for (int i = 0; i < argc; i++)
+    // Si la première commande est "For", on lance une boucle
+    if (argv[0] != NULL && strcmp(argv[0], "for") == 0)
     {
-        // Vérifie si l'argument courant est un pipe
-        if (argv[i] != NULL && strcmp(argv[i], "|") == 0)
-        {
-            return parse_and_execute_pipe(argc, argv);
-        }
+        loop_options *options = option_struc(argc, argv);
+        return loop_function(argv[3], argv, argc, options);
+    }
+
+    if (argv[0] != NULL && strcmp(argv[0], "if") == 0)
+    {
+        // TODO : Implémenter la commande if
+        return if_function(argc, argv);
     }
 
     // Parcours des arguments pour détecter une structure de commande
@@ -46,17 +49,14 @@ int parse_and_execute(int argc, char **argv)
         }
     }
 
-    // Si la première commande est "For", on lance une boucle
-    if (argv[0] != NULL && strcmp(argv[0], "for") == 0)
+    // Parcours des arguments pour détecter un pipe (|) et rediriger l'exécution
+    for (int i = 0; i < argc; i++)
     {
-        loop_options *options = option_struc(argc, argv);
-        return loop_function(argv[3], argv, argc, options);
-    }
-
-    if (argv[0] != NULL && strcmp(argv[0], "if") == 0)
-    {
-        // TODO : Implémenter la commande if
-        return if_function(argc, argv);
+        // Vérifie si l'argument courant est un pipe
+        if (argv[i] != NULL && strcmp(argv[i], "|") == 0)
+        {
+            return parse_and_execute_pipe(argc, argv);
+        }
     }
 
     // Commande externe : appel à la fonction pour exécuter la commande simple
